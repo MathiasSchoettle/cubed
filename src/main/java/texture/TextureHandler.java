8f31b676ff4663571a7b50f5f5ad4b9afd557927
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2025-08-29 00:50:37 +0200

    added support for texture arrays

---
+++ src/main/java/texture/TextureHandler.java
@@ -0,0 +1,66 @@
+package texture;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL12.*;
+import static org.lwjgl.opengl.GL20.glGenTextures;
+import static org.lwjgl.opengl.GL30.GL_TEXTURE_2D_ARRAY;
+import static org.lwjgl.opengl.GL42.glTexStorage3D;
+
+public class TextureHandler {
+
+    private final Map<String, Integer> textureIds = new HashMap<>();
+
+    public void loadTextureArray(String name, int width, int height, List<int[]> textureData) {
+
+        unload(name);
+
+        int textureArray = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D_ARRAY, textureArray);
+
+        glTexStorage3D(GL_TEXTURE_2D_ARRAY, 1, GL_RGBA8, width, height, textureData.size());
+
+        for (int layer = 0; layer < textureData.size(); ++layer) {
+            var image = textureData.get(layer);
+            glTexSubImage3D(
+                    GL_TEXTURE_2D_ARRAY,
+                    0,
+                    0,
+                    0,
+                    layer,
+                    width,
+                    height,
+                    1,
+                    GL_BGRA,
+                    GL_UNSIGNED_BYTE,
+                    image
+            );
+        }
+
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+        glBindTexture(GL_TEXTURE_2D_ARRAY, 0);
+
+        System.out.println("Created texture array " + name + " with id " + textureArray + " containing " + textureData.size() + " textures");
+
+        textureIds.put(name, textureArray);
+    }
+
+    public void unload(String name) {
+        var texture = textureIds.remove(name);
+        if (texture != null) {
+            glDeleteTextures(texture);
+        }
+    }
+
+    public Optional<Integer> getId(String name) {
+        return Optional.of(textureIds.get(name));
+    }
+}
---
+++ src/main/java/texture/TextureManager.java
@@ -0,0 +1,62 @@
+package texture;
+
+import utils.filesystem.FileLoader;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.lwjgl.opengl.GL11.glBindTexture;
+import static org.lwjgl.opengl.GL30.GL_TEXTURE_2D_ARRAY;
+
+public class TextureManager {
+
+    private final TextureHandler textureHandler;
+    private final FileLoader fileLoader;
+    private final String pathPrefix;
+
+    public TextureManager(TextureHandler textureHandler, FileLoader fileLoader, String pathPrefix) {
+        this.textureHandler = textureHandler;
+        this.fileLoader = fileLoader;
+        this.pathPrefix = pathPrefix;
+    }
+
+    public void loadTextureArray(String name, int width, int height, String ...textureFiles) {
+
+        if (textureFiles.length == 0) {
+            System.err.println("Unable to load texture array: " + name + ", no textures provided");
+            return;
+        }
+
+        List<BufferedImage> images = new ArrayList<>();
+
+        for (var textureFile : textureFiles) {
+            var image = fileLoader.image(pathPrefix + "/" + textureFile);
+
+            if (image.isEmpty()) {
+                System.err.println("Unable to load texture array: " + name + ", texture: " + textureFile + " could not be loaded");
+                return;
+            }
+
+            images.add(image.get());
+        }
+
+        var incorrectImages = images.stream().filter(image -> image.getWidth() != width || image.getHeight() != height).toList();
+        if (!incorrectImages.isEmpty()) {
+            System.err.println("Not all textures had the correct size for texture array: " + name);
+            incorrectImages.forEach(image -> System.err.println(image.getWidth() + " - " + image.getHeight()));
+            return;
+        }
+
+        var textureData = images.stream()
+                .map(image -> image.getRGB(0, 0, width, height, null, 0, width))
+                .toList();
+
+        textureHandler.loadTextureArray(name, width, height, textureData);
+    }
+
+    // FIXME this is just temporary for testing
+    public void bind(String name) {
+        textureHandler.getId(name).ifPresent((id) -> glBindTexture(GL_TEXTURE_2D_ARRAY, id));
+    }
+}
